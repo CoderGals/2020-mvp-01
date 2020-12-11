b473898388de0763d9562147e8a61dec212cce6d
kambo.najada <najada@newmedia.al>
2020-12-12 00:37:24 +0100

    wip

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,12 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/search', [App\Http\Controllers\HomeController::class, 'search'])->name('search');
-Route::get('/add-to-cart/{gift}', App\Http\Controllers\CartController::class)->name('add.cart');
-Route::get('/remove-from-cart/{gift}', [App\Http\Controllers\CartController::class, 'remove'])->name('remove.cart');
-Route::get('/lower-quantity/{gift}', [App\Http\Controllers\CartController::class, 'lowerQuantity'])->name('lower.quantity.cart');
-Route::get('/cart/index', [App\Http\Controllers\CartController::class, 'index'])->name('cart.index');
-Route::get('/reset', [App\Http\Controllers\CartController::class, 'resetCard'])->name('reset.card');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/search', [App\Http\Controllers\HomeController::class, 'search'])->name('search');
+    Route::get('/add-to-cart/{gift}', App\Http\Controllers\CartController::class)->name('add.cart');
+    Route::get('/remove-from-cart/{gift}', [App\Http\Controllers\CartController::class, 'remove'])->name('remove.cart');
+    Route::get('/lower-quantity/{gift}', [App\Http\Controllers\CartController::class, 'lowerQuantity'])->name('lower.quantity.cart');
+    Route::get('/cart/index', [App\Http\Controllers\CartController::class, 'index'])->name('cart.index');
+    Route::get('/reset', [App\Http\Controllers\CartController::class, 'resetCard'])->name('reset.card');
+});
