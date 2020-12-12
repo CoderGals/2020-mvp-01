f0a82c3e50d5ade8ee2aa0ae0a398b8082e8b446
kambo.najada <najada@newmedia.al>
2020-12-12 20:30:32 +0100

    wip

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,70 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+<section id="auth-login" class="row flexbox-container">
+    <div class="col-xl-8 col-11 m-auto">
+        <div class="card bg-authentication mb-0">
+            <div class="row m-0">
+                <!-- left section-login -->
+                <div class="col-md-6 col-12 px-0 pt-0">
+                    <div class="card disable-rounded-right mb-0 p-2 h-100 d-flex justify-content-center">
+                        <div class="card-header pb-1">
+                            <div class="card-title justify-content-center  pb-3">
+                                <h3 style="text-align:center;color:red;font-size:39px;font-weight:bold">
+                                    Gift App
+                                </h3>
                             </div>
                         </div>
+                        <div class="card-content">
+                            <div class="card-body">
+                                <div class="divider">
+                                    <div class="divider-text text-uppercase text-muted">
+                                        <small>Login with email</small>
+                                    </div>
+                                </div>
+                                <form method="POST" action="{{ route('login') }}">
+                                    @csrf
+                                    <div class="form-group mb-50">
+                                        <label class="text-bold-600 tt-u" for="exampleInputEmail1">Email address</label>
+                                        <input type="email" name="email" class="form-control" id="exampleInputEmail1" placeholder="Email address">
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="text-bold-600 tt-u" for="exampleInputPassword1">Password</label>
+                                        <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                                    </div>
+                                    <div class="form-group d-flex flex-md-row flex-column justify-content-between align-items-center">
+                                        <div class="text-left">
+                                            <div class="checkbox checkbox-sm">
+                                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                                <label class="checkboxsmall" for="exampleCheck1">
+                                                    <small>Keep me logged in</small>
+                                                </label>
+                                            </div>
+                                        </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
+                                    </div>
+                                    <button type="submit" class="btn btn-danger glow w-100 position-relative">Login
+                                        <i id="icon-arrow" class="bx bx-right-arrow-alt"></i>
+                                    </button>
+                                </form>
+                                <hr>
+                                <div class="text-center">
+                                    <small class="mr-25" style="color: #727E8C;">Don't have an account?</small>
+                                    <a href="{{asset('register')}}"><small>Sign up</small></a>
                                 </div>
                             </div>
                         </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+                    </div>
+                </div>
+                <!-- right section image -->
+                <div class="col-md-6 d-md-block d-none text-center align-self-center p-3">
+                    <div class="card-content">
+                        <img class="img-fluid" style="height: 100%;width: auto;object-fit: cover" src="{{asset('img/login_cover.jpeg')}}" alt="branding logo">
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+</section>
+
 @endsection
--- resources/views/gift.blade.php
+++ resources/views/gift.blade.php
@@ -1,6 +1,6 @@
 <div class="col-md-2 mt-2 gift-box">
     <div class="">
-       
+
 
         <div class="gift-box-body  wow fadeIn">
             <img class="w-100 h-auto" src="{{  $gift->pic_url }}" />
@@ -9,27 +9,27 @@
                 <a href="{{ route('add.favourites', $gift) }}"> <i class="fa fa-star" style="color:green"></i> </a>
             </div>
         </div>
-        
+
         <p>{{ $gift->description }}</p>
-        
+
         <div class="gift-box-footer">
-                <span class="d-block" style="float:left">{{ $gift->price }} $</span>
-                @if(isset(session()->get('cart')[$gift->id]))
-                    <span class="d-block" style="float:right">x{{ session()->get('cart')[$gift->id]['quantity'] }} on cart</span>
-                    <br />
-                    <div class="d-flex justify-content-between mt-3">
-                        <div class="d-flex justify-content-start">
-                            <a href="{{ route('add.cart', $gift) }}" class="btn btn-outline-success pull-right" style="float:right;padding: 14px;"><i class="fa fa-plus"></i></a>
-                            <a href="{{ route('lower.quantity.cart', $gift) }}" class="btn btn-outline-primary pull-right ml-1" style="padding: 14px;"><i class="fa fa-minus"></i></a>
-                        </div>
-                        <a href="{{ route('remove.cart', $gift) }}" class="btn btn-outline-danger pull-right" style="float:right;padding: 14px;"><i class="fa fa-trash"></i></a>
-                    </div>
-                @else
-                    <div>
-                        <a href="{{ route('add.cart', $gift) }}" class="btn btn-outline-success pull-right" style="float:right;padding: 14px;">Add to cart</a>
-                    </div>
-                @endif
+            <span class="d-block" style="float:left">{{ $gift->price }} $</span>
+            @if(isset(session()->get('cart')[$gift->id]))
+            <span class="d-block" style="float:right">x{{ session()->get('cart')[$gift->id]['quantity'] }} on cart</span>
+            <br />
+            <div class="d-flex justify-content-between mt-3">
+                <div class="d-flex justify-content-start">
+                    <a href="{{ route('add.cart', $gift) }}" class="btn btn-outline-success pull-right" style="float:right;padding: 14px;"><i class="fa fa-plus"></i></a>
+                    <a href="{{ route('lower.quantity.cart', $gift) }}" class="btn btn-outline-primary pull-right ml-1" style="padding: 14px;"><i class="fa fa-minus"></i></a>
+                </div>
+                <a href="{{ route('remove.cart', $gift) }}" class="btn btn-outline-danger pull-right" style="float:right;padding: 14px;"><i class="fa fa-trash"></i></a>
+            </div>
+            @else
+            <div>
+                <a href="{{ route('add.cart', $gift) }}" class="btn btn-outline-success pull-right" style="float:right;padding: 14px;">Add to cart</a>
+            </div>
+            @endif
         </div>
-        
+
     </div>
 </div>
